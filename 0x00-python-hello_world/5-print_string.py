a77b2a3d313ed0c2c02b955423322dc13d1b1e7a
phetex <eassam89@gmail.com>
2022-11-28 17:24:06 -0800

    all tasks

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/bin/bash
+str = "Holberton School"
+print(3 * str)
+print(str[:9])
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/bin/bash
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
