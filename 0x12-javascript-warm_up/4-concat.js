bc701cf826ad0f09367983532b9a88d0418d9930
phetex <eassam89@gmail.com>
2023-05-11 11:27:13 +0000

    javascript

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
 
 console.log(process.argv[2] + ' is ' + process.argv[3]);
+// Check if any argument is passed
+if (process.argv[2] !== undefined) {
+  // Print the first argument
+  console.log(process.argv[2]);
+} else {
+  // No argument is passed
+  console.log("No argument");
+}
+
